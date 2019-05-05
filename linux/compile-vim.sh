24fb6b6af47f35f36688e520784f7a56bb3c1a90
Steffen Urbanski <sturbi@sturbi.de>
2019-05-05 15:00:33 +0200

    add some vim 8.1 stuff

---
+++ linux/compile-vim.sh
@@ -0,0 +1,17 @@
+./configure --with-features=huge \
+--enable-multibyte \
+--enable-rubyinterp=yes \
+--enable-pythoninterp=yes \
+--with-python-config-dir=/lib64/python2.7/config \
+--enable-perlinterp=yes \
+--enable-luainterp=yes \
+--enable-gui=gtk2 \
+--enable-gnome-check \
+--enable-gtk3-check \
+--enable-cscope \
+--enable-fontset \
+--enable-autoservername \
+--enable-cscope \
+--enable-largefile \
+--enable-fail-if-missing \
+--prefix=/usr/local
