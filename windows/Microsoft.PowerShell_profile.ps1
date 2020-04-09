2bd47be8f2f6d60b12cd860e42fafa54ae5d9263
Steffen Urbanski <s.urbanski@itp-05-nb.bnotk.de>
2020-04-09 15:15:16 +0200

    add vCenter to Prompt

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,15 @@
 $env:PSModulePath = $env:PSModulePath + ";C:\data\WindowsPowerShell\Modules" 
 
+Add-PSSnapin VeeamPSSnapin
 Add-PSSnapin Quest.ActiveRoles.ADManagement
 
 Import-Module BitsTransfer
- 
 
 $lib = "C:\data\WindowsPowerShell\lib\"
 $pstools = "C:\data\WindowsPowerShell\PSTools"
 
-#set-alias -Name Edit-File -Value "C:\Program Files (x86)\Notepad++\notepad++.exe" -Force
-#set-alias -Name e -Value "C:\Program Files (x86)\Notepad++\notepad++.exe" -Force
-#set-alias -Name Edit-File -Value "C:\Program Files\SAPIEN Technologies, Inc\PrimalPad 2014\PrimalPad.exe" -Force
-#set-alias -Name e -Value "C:\Program Files\SAPIEN Technologies, Inc\PrimalPad 2014\PrimalPad.exe" -Force
-set-alias -Name Edit-File -Value "C:\Program Files\Sublime Text 3\sublime_text.exe" -Force
-set-alias -Name e -Value "C:\Program Files\Sublime Text 3\sublime_text.exe" -Force
-set-alias -Name subl -Value "C:\Program Files\Sublime Text 3\sublime_text.exe" -Force
-
-Set-Alias -Name scp -Value "C:\Program Files\VanDyke Software\ClientPack\vcp" -Force
-Set-Alias -Name ssh -Value "C:\Program Files\VanDyke Software\ClientPack\vsh" -Force
-Set-Alias -Name tf -Value "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\tf.exe" -Force
-
-# . "C:\data\WindowsPowerShell\lib\ssh.ps1"
 . "C:\data\WindowsPowerShell\lib\sign.ps1"
-# . "C:\data\WindowsPowerShell\lib\topmost.ps1"
 . "C:\data\WindowsPowerShell\lib\loads.ps1"
-. "C:\data\WindowsPowerShell\lib\Out-Excel.ps1"
 
 $def = @"
 public class ClientCertWebClient : System.Net.WebClient
@@ -55,33 +40,44 @@ public class ClientCertWebClient : System.Net.WebClient
 }
 "@
 Add-Type -TypeDefinition $def
-$wc = New-Object ClientCertWebClient
-$certs = dir cert:\CurrentUser\My
-$wc.AddCerts($certs)
-$wcresult = $wc.DownloadString("https://localhost:12984/keepass/entry/E048F0F148805F47A6D9D2C8658189CB")
-$credjsonstring = $wcresult | ConvertFrom-Json
-$username = $credjsonstring.Username
-$password = $credjsonstring.Password
-$acred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
-Connect-QADService int.gematik.de -Credential $acred
-
-# cleanup cleartext variables
-# Remove-Variable $username 
-# Remove-Variable $password
-# Remove-Variable $wcresult
-# Remove-Variable $credjsonstring
+function vcsa-admin {
+    Clear-Variable username -ErrorAction SilentlyContinue
+    Clear-Variable password -ErrorAction SilentlyContinue
+    Clear-Variable wcresult -ErrorAction SilentlyContinue
+    Clear-Variable wc -ErrorAction SilentlyContinue
+    Clear-Variable credjsonstring -ErrorAction SilentlyContinue
+    $wc = New-Object ClientCertWebClient
+    $certs = dir cert:\CurrentUser\My
+    $wc.AddCerts($certs)
+    $wcresult = $wc.DownloadString("https://localhost:12984/keepass/entry/6A6D8747428B614DBE021470D5DE8FF6")
+    $credjsonstring = $wcresult | ConvertFrom-Json
+    $username = $credjsonstring.Username
+    $password = $credjsonstring.Password
+    connect-viserver vcsa-01 -User $username -Password $password
+    Clear-Variable username -ErrorAction SilentlyContinue
+    Clear-Variable password -ErrorAction SilentlyContinue
+    Clear-Variable wcresult -ErrorAction SilentlyContinue
+    Clear-Variable wc -ErrorAction SilentlyContinue
+    Clear-Variable credjsonstring -ErrorAction SilentlyContinue
+}
 
 function global:prompt {
     # our theme
     $cdelim = [ConsoleColor]::DarkCyan
     $chost = [ConsoleColor]::Green
     $cloc = [ConsoleColor]::Cyan
-    write-host "$([char]0x0A7) " -n -f $cloc
-    write-host ([net.dns]::GetHostName()) -n -f $chost
-    write-host ' {' -n -f $cdelim
-    # write-host (shorten-path (pwd).Path) -n -f $cloc
-	write-host (pwd).Path -n -f $cloc
-    write-host '}'  -f $cdelim
+    $cvcenter = [ConsoleColor]::Yellow
+    write-host "$([char]0x0A7) " -NoNewline -ForegroundColor $cloc
+    $PSName = '(' + $PSVersion + ') '
+    Write-Host $PSName -NoNewline
+    write-host ([net.dns]::GetHostName()) -NoNewline -ForegroundColor $chost
+    if ($global:DefaultVIServer) {
+        $vcenterstring = ' [' + $global:DefaultVIServer.User + '@' + $global:DefaultVIServer.Name + ']'
+        write-host $vcenterstring -NoNewline -ForegroundColor $cvcenter
+    }
+    write-host ' {' -NoNewline -ForegroundColor $cdelim
+	write-host (Get-Location).Path -NoNewline -ForegroundColor $cloc
+    write-host '}'  -ForegroundColor $cdelim
 	Write-Host ":~#" -NoNewline -ForegroundColor $cdelim
     return ' '
 }
@@ -123,11 +119,3 @@ $newsize.height = 24
 $newsize.width = 80
 $pswindow.buffersize = $newsize
 }
-
-# if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
-
-# Load posh-git example profile
-# . 'C:\tools\poshgit\dahlbyk-posh-git-869d4c5\profile.example.ps1'
-
-# Rename-Item Function:\Prompt PoshGitPrompt -Force
-# function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
